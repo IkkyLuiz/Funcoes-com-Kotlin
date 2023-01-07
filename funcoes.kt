6de924fd2c888bbedbc2f5873000fdb1264e85f0
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2023-01-07 14:55:48 -0300

    Add files via upload
    Funções em Kotlin.

---
+++ funcoes.kt
@@ -0,0 +1,38 @@
+/**
+ * Valores de parâmetro padrão e argumentos nomeados.
+ *
+ * @see [Functions](https://play.kotlinlang.org/byExample/01_introduction/02_Functions)
+ */
+
+fun printMessage(message: String): Unit {                               // 1
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {  // 2
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {                                          // 3
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y                                    // 4
+
+fun division(x: Int, y: Int) = x/y
+fun minus(x:Int, y: Int) = x-y
+
+fun main() {
+    printMessage("Hello")                                               // 5      
+     printMessageWithPrefix("Hello", "Log")                              // 6
+      printMessageWithPrefix("Hello", "Log")                              // 6
+    printMessageWithPrefix("Hello")                                     // 7
+    printMessageWithPrefix(prefix = "Log", message = "Hello")           // 8
+    println(sum(1, 2))                                                  // 9
+    println(multiply(2, 4)) 
+    println(division(10,5))                                             //10
+    println(minus(9,6))                                                 //11
+ 
+
+   
+                                               
+}
